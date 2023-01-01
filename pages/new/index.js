35975c16972d7a2c450d040a1677e00b962f20c6
geyler <geylerps@gmail.com>
2023-01-01 17:31:15 -0300

    test

--- pages/new/index.js
+++ pages/new/index.js
@@ -1,53 +1,59 @@
-import Head from 'next/head'
-import Header from '../../componentes/Header';
-import Navegacion from '../../componentes/Navegacion';
-import NavMovil from '../../componentes/NavMovil';
-import { useState, useEffect } from 'react';
-import CategoriasMenu from '../../componentes/CategoriasMenu';
-import Footer from '../../componentes/Footer';
+import Head from "next/head";
+import Header from "../../componentes/Header";
+import Navegacion from "../../componentes/Navegacion";
+import NavMovil from "../../componentes/NavMovil";
+import { useState, useEffect } from "react";
+import CategoriasMenu from "../../componentes/CategoriasMenu";
+import Footer from "../../componentes/Footer";
 
-export default function Pag(props) {
-
+export default function Pag() {
   const [active, setActive] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(min-width: 769px)');
+    const mediaQuery = window.matchMedia("(min-width: 769px)");
     setActive(mediaQuery.matches);
   }, []);
 
   const openClose = () => {
     if (active) {
-      setActive(false)
+      setActive(false);
     } else {
-      setActive(true)
+      setActive(true);
     }
-  }
+  };
 
   return (
-    <div className='contenedor-app'>
+    <div className="contenedor-app">
       <Head>
-        <title>We have not found what you are looking for, try looking for something else. PORNAZOS.COM</title>
-        <meta name="description" content='We have not found what you are looking for, try looking for something else. PORNAZOS.COM' />
+        <title>
+          We have not found what you are looking for, try looking for something
+          else. PORNAZOS.COM
+        </title>
+        <meta
+          name="description"
+          content="We have not found what you are looking for, try looking for something else. PORNAZOS.COM"
+        />
       </Head>
 
-      <div className={`contenedor ${active ? 'active' : ''}`} id="contenedor">
-
+      <div className={`contenedor ${active ? "active" : ""}`} id="contenedor">
         <Header click={openClose} active={active} />
         <Navegacion click={openClose} active={active} />
         <NavMovil click={openClose} active={active} />
 
-        <main className={`main ${active ? active : ''}`}>
+        <main className={`main ${active ? active : ""}`}>
+          <div className="content-no-videos">
+            <div className="no-videos">
+              The result was not found, look for something shorter or click on a
+              category in the menu below.
+            </div>
+            <div className="no-videos-tags">
+              <CategoriasMenu />
+            </div>
+          </div>
 
-          
-             <div className='content-no-videos'>
-                <div className='no-videos'>The result was not found, look for something shorter or click on a category in the menu below.</div>
-                <div className='no-videos-tags'><CategoriasMenu /></div>
-              </div> 
-          
-          
-          <Footer/>
+          <Footer />
         </main>
       </div>
     </div>
-  )
+  );
 }
